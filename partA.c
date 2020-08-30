b0fd773b3f22b320b349919a8569b71ae679e885
tuj83407 <tuj83407@temple.edu>
2020-08-30 01:51:06 -0400

    partA complete ready to test

--- partA.c
+++ partA.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 int main() {
-	car filename[];
+	char filename[100];
+	char filecontent[100];
 	printf("enter folder directory and filename");
 	scanf("%s", &filename);
-	FILE *fp = fopen("%s", 
-	fopen(
+	FILE *fp = fopen("%s", filename, r);
+	if(fp == NULL){
+		printf("cannot open file\n");
+	}
+	while(fgets(filecontent, 100, fp) != NULL){
+		printf("%s", filecontent);
+	}
+	fclose(fp);
 	
 	return 0;
 }
