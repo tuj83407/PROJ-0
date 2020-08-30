a494c6c9b8d31e5ce5e6aab7fd75262c440dd99f
tuj83407 <tuj83407@temple.edu>
2020-08-29 23:51:00 -0400

    started partA

--- partA.c
+++ partA.c
@@ -1,1 +1,10 @@
-a
+#include <stdio.h>
+int main() {
+	car filename[];
+	printf("enter folder directory and filename");
+	scanf("%s", &filename);
+	FILE *fp = fopen("%s", 
+	fopen(
+	
+	return 0;
+}
